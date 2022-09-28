7b8c43747bc350de253e58d564734d982dad432f
nafilanajlaa <najlanafilaaa27@gmail.com>
2022-09-28 07:34:50 +0700

    porto

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center:true,
+      padding:'16px',
+    },
+    extend: {
+      colors:{
+        primary:'#f472b6',
+        dark: '#0f172a',
+        abu:'#64748b',
+    },
+    screens:{
+      '2xl':'1320px',
+    },
+  },
+},
+  plugins: [],
+};
